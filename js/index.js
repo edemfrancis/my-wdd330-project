c4ce18b0968a9dd9c62d1978269698d1bdcd1ee3
edemfrancis <francisedem2023@gmail.com>
2024-10-22 18:28:54 +0100

    Added files

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const myName = document.createElement("h1");
-myName.textContent = "Edem Francis";
+import { createElement } from "./utilis";
+
+const myName = createElement("h1", {textContent: "Hello World, Edem Francis"});
 
 document.getElementById("root").appendChild(myName);
